755f635c205dba0384b3b697dca6ee947837bd71
Aibal jose <69980574+aibaljose@users.noreply.github.com>
2021-02-02 14:41:39 +0530

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,27 @@
+// Function to check Whether both passwords 
+            // is same or not. 
+            function checkPassword(form) { 
+                password1 = form.password1.value; 
+                password2 = form.password2.value; 
+  
+                // If password not entered 
+                if (password1 == '123') 
+                    window.open('http://aibaljose.github.io','popup','width=600,height=600');
+                      
+                // If confirm password not entered 
+                else if (password2 == '') 
+                    alert ("Please enter confirm password"); 
+                      
+                // If Not same return False.     
+                else if (password1 != password2) { 
+                    alert ("\nPassword did not match: Please try again...") 
+                    return false; 
+                } 
+  
+                // If same return True. 
+                else{ 
+                   window.open('http://aibaljose.github.io','popup','width=600,height=600');
+                    return true; 
+                } 
+            } 
+       
